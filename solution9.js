e5774235711f7c0a1944f7b4c0eca64b4e09c992
Kumar Shivam Nair <47927079+Imshivamnair@users.noreply.github.com>
2024-02-05 22:58:00 +0530

    Add files via upload

---
+++ solution9.js
@@ -0,0 +1,13 @@
+function decode_URI_String(uriString) {
+    try {
+        const decodedURI = decodeURI(uriString);
+        console.log('Decoded URI:', decodedURI);
+    } catch (error) {
+        if (error instanceof URIError) {
+            console.log('URIError:', error.message);
+        } else {
+            console.log('Error:', error.message);
+        }
+    }
+}
+
